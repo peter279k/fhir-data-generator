6169bd620940b2d196cc041f57130592c75065c9
peter279k <peter279k@gmail.com>
2023-12-21 16:29:45 +0800

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='fhir-data-generator',
+      version='0.2.0',
+      description='The FHIR data generator is compatible with the some MI-TW scenarios',
+      url='https://github.com/peter279k/fhir-data-generator',
+      author='peter279k',
+      author_email='peter279k@gmail.com',
+      packages=['FHIR', 'Data'],
+      license='MIT',
+      python_requires=">=3.8",
+      zip_safe=False)
