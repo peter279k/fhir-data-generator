4f732e69c27be746dccef42e08f1d8cd41fb5394
peter279k <peter279k@gmail.com>
2024-07-10 13:28:39 +0800

    Release 0.2.0-test

--- fhir_data_generator/__init__.py
+++ fhir_data_generator/__init__.py
@@ -2,5 +2,5 @@ from fhir_data_generator.patient.patient import Patient
 from fhir_data_generator.simple_client.http import Http
 
 
-__version__ = '0.1.1'
+__version__ = '0.2.0-test'
 __all__ = ['Patient', 'Http']
