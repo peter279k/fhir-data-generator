150180b4258b62aebd88cf4f8b9a1f72d06bebbd
peter279k <peter279k@gmail.com>
2024-08-23 17:56:28 +0800

    Complete Practitioner & PractitionerRole consumer

--- web/front_end/js/track1.2024.js
+++ web/front_end/js/track1.2024.js
@@ -380,7 +380,16 @@ async function doGeneratePractitionerRoleRequest(trackServerEndpoint, oauthServe
         }),
     }).done((data) => {
         let jsonData = data.json;
+        if (jsonData.total === 0) {
+            errorMessage['text'] = '尚未找到任何筆數！';
+            Swal.fire(errorMessage);
+            return false;
+        }
         let practitionerRoleResource = jsonData;
+        if (jsonData.entry) {
+            practitionerRoleResource = jsonData.entry[0].resource;
+        }
+
         if (data.status !== 200 && data.status !== 201) {
             let htmlErrorMessage = `
                 <p>error; HTTP status code: ${data.status}</p>
