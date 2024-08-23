62338b224c8d0b1504422e1bb1a4366a1f47881d
peter279k <peter279k@gmail.com>
2024-08-23 18:48:46 +0800

    Complete DocumentReference consumer query

--- web/front_end/js/track1.2024.js
+++ web/front_end/js/track1.2024.js
@@ -883,7 +883,16 @@ async function doGenerateDocumentReferenceRequest(trackServerEndpoint, oauthServ
         }),
     }).done((data) => {
         let jsonData = data.json;
+        if (jsonData.total === 0) {
+            errorMessage['text'] = '尚未找到任何筆數！';
+            Swal.fire(errorMessage);
+            return false;
+        }
         let documentReferenceResource = jsonData;
+        if (jsonData.entry) {
+            documentReferenceResource = jsonData.entry[0].resource;
+        }
+
         if (data.status !== 200 && data.status !== 201) {
             let htmlErrorMessage = `
                 <p>error; HTTP status code: ${data.status}</p>
