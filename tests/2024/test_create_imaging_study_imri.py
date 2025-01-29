e99e3c867858349ff8bbb1ded7b43a1f4df3056d
peter279k <peter279k@gmail.com>
2025-01-30 01:31:49 +0800

    Fix ImagingStudy for IMRI test

--- tests/2024/test_create_imaging_study_imri.py
+++ tests/2024/test_create_imaging_study_imri.py
@@ -1,12 +1,10 @@
-from fixtures.imaging_study_2024 import *
+from fixtures.imaging_study_imri_2024 import *
 
 
-def test_create(imaging_study_class, profile_urls, identifier, status, subject, encounter, started, number_of_series, number_of_instances, procedure_reference, procedure_code_coding, series_uid, series_modality, series_body_site, series_performer_actor, series_instance_uid, series_instance_sop_class, expected_payload):
+def test_create(imaging_study_class, profile_urls, status, subject, encounter, started, interpreter, description, expected_payload):
     expected = expected_payload
 
     imaging_study_class.set_profile_urls(profile_urls)
-
-    imaging_study_class.set_identifier(identifier)
 
     imaging_study_class.set_status(status)
 
@@ -16,20 +14,9 @@ def test_create(imaging_study_class, profile_urls, identifier, status, subject, 
 
     imaging_study_class.set_started(started)
 
-    imaging_study_class.set_number_of_series(number_of_series)
+    imaging_study_class.set_interpreter(interpreter)
 
-    imaging_study_class.set_number_of_instances(number_of_instances)
-
-    imaging_study_class.set_procedure_reference(procedure_reference)
-
-    imaging_study_class.set_procedure_code_coding(procedure_code_coding)
-
-    imaging_study_class.set_series_uid(series_uid)
-    imaging_study_class.set_series_modality(series_modality)
-    imaging_study_class.set_series_body_site(series_body_site)
-    imaging_study_class.set_series_performer_actor(series_performer_actor)
-    imaging_study_class.set_series_instance_uid(series_instance_uid)
-    imaging_study_class.set_series_instance_sop_class(series_instance_sop_class)
+    imaging_study_class.set_description(description)
 
     imaging_study_class.create()
 
