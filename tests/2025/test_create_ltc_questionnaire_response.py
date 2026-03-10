3bf4b5f72039165ca62eabf4d94d378dc6106190
peter279k <peter279k@gmail.com>
2026-03-10 17:05:47 +0800

    Add QuestionnaireResponse generator for MMSE test

--- tests/2025/test_create_ltc_questionnaire_response.py
+++ tests/2025/test_create_ltc_questionnaire_response.py
@@ -1,18 +1,20 @@
 from fixtures.questionnaire_response_ltc import *
 
 
-def test_create_moderate_example(questionnaire_response_classes, profile_urls, extensions, questionnaire, status, subject, authored_lists, author, source, items, expected_payload):
+def test_create_cdr_moderate_example(questionnaire_response_classes, profile_urls, extensions, questionnaires, status, subject, authored_lists, author, source, items, expected_payload):
     expected = expected_payload
     expected_payload['id'] = questionnaire_response_classes[0].response_id
     expected['extension'] = extensions[0]
     expected['authored'] = authored_lists[0]
     expected['item'] = items[0]
+    expected['meta']['profile'] = profile_urls[0]
+    expected['questionnaire'] = questionnaires[0]
 
-    questionnaire_response_classes[0].set_profile_urls(profile_urls)
+    questionnaire_response_classes[0].set_profile_urls(profile_urls[0])
 
     questionnaire_response_classes[0].set_extension(extensions[0])
 
-    questionnaire_response_classes[0].set_questionnaire(questionnaire)
+    questionnaire_response_classes[0].set_questionnaire(questionnaires[0])
 
     questionnaire_response_classes[0].set_status(status)
 
@@ -30,18 +32,20 @@ def test_create_moderate_example(questionnaire_response_classes, profile_urls, e
 
     assert questionnaire_response_classes[0].payload_template == expected
 
-def test_create_example(questionnaire_response_classes, profile_urls, extensions, questionnaire, status, subject, authored_lists, author, source, items, expected_payload):
+def test_create_cdr_example(questionnaire_response_classes, profile_urls, extensions, questionnaires, status, subject, authored_lists, author, source, items, expected_payload):
     expected = expected_payload
     expected_payload['id'] = questionnaire_response_classes[1].response_id
     expected['extension'] = extensions[1]
     expected['authored'] = authored_lists[1]
     expected['item'] = items[1]
+    expected['meta']['profile'] = profile_urls[1]
+    expected['questionnaire'] = questionnaires[1]
 
-    questionnaire_response_classes[1].set_profile_urls(profile_urls)
+    questionnaire_response_classes[1].set_profile_urls(profile_urls[1])
 
     questionnaire_response_classes[1].set_extension(extensions[1])
 
-    questionnaire_response_classes[1].set_questionnaire(questionnaire)
+    questionnaire_response_classes[1].set_questionnaire(questionnaires[1])
 
     questionnaire_response_classes[1].set_status(status)
 
@@ -59,18 +63,20 @@ def test_create_example(questionnaire_response_classes, profile_urls, extensions
 
     assert questionnaire_response_classes[1].payload_template == expected
 
-def test_create_complete_example(questionnaire_response_classes, profile_urls, extensions, questionnaire, status, subject, authored_lists, author, source, items, expected_payload):
+def test_create_cdr_complete_example(questionnaire_response_classes, profile_urls, extensions, questionnaires, status, subject, authored_lists, author, source, items, expected_payload):
     expected = expected_payload
     expected_payload['id'] = questionnaire_response_classes[2].response_id
     expected['extension'] = extensions[2]
     expected['authored'] = authored_lists[2]
     expected['item'] = items[2]
+    expected['meta']['profile'] = profile_urls[1]
+    expected['questionnaire'] = questionnaires[1]
 
-    questionnaire_response_classes[2].set_profile_urls(profile_urls)
+    questionnaire_response_classes[2].set_profile_urls(profile_urls[1])
 
     questionnaire_response_classes[2].set_extension(extensions[2])
 
-    questionnaire_response_classes[2].set_questionnaire(questionnaire)
+    questionnaire_response_classes[2].set_questionnaire(questionnaires[1])
 
     questionnaire_response_classes[2].set_status(status)
 
@@ -87,3 +93,100 @@ def test_create_complete_example(questionnaire_response_classes, profile_urls, e
     questionnaire_response_classes[2].create()
 
     assert questionnaire_response_classes[2].payload_template == expected
+
+def test_create_mmse_example(questionnaire_response_classes, profile_urls, extensions, questionnaires, status, subject, authored_lists, author, source, mmse_items, expected_payload):
+    items = mmse_items
+    expected = expected_payload
+    expected_payload['id'] = questionnaire_response_classes[3].response_id
+    expected['extension'] = extensions[3]
+    expected['authored'] = authored_lists[3]
+    expected['item'] = items[0]
+    expected['meta']['profile'] = profile_urls[1]
+    expected['questionnaire'] = questionnaires[1]
+
+    questionnaire_response_classes[3].set_profile_urls(profile_urls[1])
+
+    questionnaire_response_classes[3].set_extension(extensions[3])
+
+    questionnaire_response_classes[3].set_questionnaire(questionnaires[1])
+
+    questionnaire_response_classes[3].set_status(status)
+
+    questionnaire_response_classes[3].set_subject(subject)
+
+    questionnaire_response_classes[3].set_authored(authored_lists[3])
+
+    questionnaire_response_classes[3].set_author(author)
+
+    questionnaire_response_classes[3].set_source(source)
+
+    questionnaire_response_classes[3].set_item(items[0])
+
+    questionnaire_response_classes[3].create()
+
+    assert questionnaire_response_classes[3].payload_template == expected
+
+def test_create_mmse_complete_example(questionnaire_response_classes, profile_urls, extensions, questionnaires, status, subject, authored_lists, author, source, mmse_items, expected_payload):
+    items = mmse_items
+    expected = expected_payload
+    expected_payload['id'] = questionnaire_response_classes[4].response_id
+    expected['extension'] = extensions[4]
+    expected['authored'] = authored_lists[4]
+    expected['item'] = items[1]
+    expected['meta']['profile'] = profile_urls[1]
+    expected['questionnaire'] = questionnaires[1]
+
+    questionnaire_response_classes[4].set_profile_urls(profile_urls[1])
+
+    questionnaire_response_classes[4].set_extension(extensions[4])
+
+    questionnaire_response_classes[4].set_questionnaire(questionnaires[1])
+
+    questionnaire_response_classes[4].set_status(status)
+
+    questionnaire_response_classes[4].set_subject(subject)
+
+    questionnaire_response_classes[4].set_authored(authored_lists[4])
+
+    questionnaire_response_classes[4].set_author(author)
+
+    questionnaire_response_classes[4].set_source(source)
+
+    questionnaire_response_classes[4].set_item(items[1])
+
+    questionnaire_response_classes[4].create()
+
+    assert questionnaire_response_classes[4].payload_template == expected
+
+def test_create_mmse_impaired_example(questionnaire_response_classes, profile_urls, extensions, questionnaires, status, subject, authored_lists, author, source, mmse_items, expected_payload):
+    items = mmse_items
+    expected = expected_payload
+    expected_payload['id'] = questionnaire_response_classes[5].response_id
+    expected['extension'] = extensions[5]
+    expected['authored'] = authored_lists[5]
+    expected['item'] = items[2]
+    expected['meta']['profile'] = profile_urls[1]
+    expected['questionnaire'] = questionnaires[1]
+
+    questionnaire_response_classes[5].set_profile_urls(profile_urls[1])
+
+    questionnaire_response_classes[5].set_extension(extensions[5])
+
+    questionnaire_response_classes[5].set_questionnaire(questionnaires[1])
+
+    questionnaire_response_classes[5].set_status(status)
+
+    questionnaire_response_classes[5].set_subject(subject)
+
+    questionnaire_response_classes[5].set_authored(authored_lists[5])
+
+    questionnaire_response_classes[5].set_author(author)
+
+    questionnaire_response_classes[5].set_source(source)
+
+    questionnaire_response_classes[5].set_item(items[2])
+
+    questionnaire_response_classes[5].create()
+
+    assert questionnaire_response_classes[5].payload_template == expected
+
--- tests/fixtures/questionnaire_response_ltc.py
+++ tests/fixtures/questionnaire_response_ltc.py
@@ -8,12 +8,18 @@ def questionnaire_response_classes():
         QuestionnaireResponse(response_id='ltc-questionnaire-response-cdr-moderate-example'),
         QuestionnaireResponse(response_id='ltc-questionnaire-response-cdr-example'),
         QuestionnaireResponse(response_id='ltc-questionnaire-response-cdr-complete-example'),
+
+        QuestionnaireResponse(response_id='ltc-questionnaire-response-mmse-example'),
+        QuestionnaireResponse(response_id='ltc-questionnaire-response-mmse-complete-example'),
+        QuestionnaireResponse(response_id='ltc-questionnaire-response-mmse-impaired-example'),
     ]
 
 @pytest.fixture
 def profile_urls():
     return [
-        'http://ltc-ig.fhir.tw/StructureDefinition/LTCQuestionnaireResponseCDR'
+        ['http://ltc-ig.fhir.tw/StructureDefinition/LTCQuestionnaireResponseCDR'],
+
+        ['http://ltc-ig.fhir.tw/StructureDefinition/LTCQuestionnaireResponseMMSE'],
     ]
 
 @pytest.fixture
@@ -31,11 +37,27 @@ def extensions():
             'url': 'http://ltc-ig.fhir.tw/StructureDefinition/cdr-total-score',
             'valueInteger': 1
         }],
+
+        [{
+            'url': 'http://ltc-ig.fhir.tw/StructureDefinition/mmse-total-score',
+            'valueInteger': 28
+        }],
+        [{
+            'url': 'http://ltc-ig.fhir.tw/StructureDefinition/mmse-total-score',
+            'valueInteger': 30
+        }],
+        [{
+            'url': 'http://ltc-ig.fhir.tw/StructureDefinition/mmse-total-score',
+            'valueInteger': 13
+        }],
     ]
 
 @pytest.fixture
-def questionnaire():
-    return 'http://ltc-ig.fhir.tw/Questionnaire/ltc-questionnaire-cdr'
+def questionnaires():
+    return [
+        'http://ltc-ig.fhir.tw/Questionnaire/ltc-questionnaire-cdr',
+        'http://ltc-ig.fhir.tw/Questionnaire/ltc-questionnaire-mmse',
+    ]
 
 @pytest.fixture
 def status():
@@ -53,6 +75,10 @@ def authored_lists():
         '2024-01-15T10:30:00+08:00',
         '2024-01-15T10:30:00+08:00',
         '2024-01-15T10:30:00+08:00',
+
+        '2024-01-15T10:30:00+08:00',
+        '2024-01-15T14:30:00+08:00',
+        '2024-01-15T14:30:00+08:00',
     ]
 
 @pytest.fixture
@@ -66,6 +92,472 @@ def source():
     return {
         'reference': 'Patient/ltc-patient-chen-ming-hui',
     }
+
+@pytest.fixture
+def mmse_items():
+    return [
+    [{
+        'linkId': 'MMSE-1',
+        'text': '今年是那一年?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-2',
+        'text': '現在是什麼季節?',
+        'answer': [{
+                'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-3',
+        'text': '今天是幾號?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-4',
+        'text': '今天是禮拜幾?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-5',
+        'text': '現在是那一個月份?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-6',
+        'text': '我們現在是在那一個省?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-7',
+        'text': '我們現在是在那一個縣、市?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-8',
+        'text': '這裡屬於哪一個區或是鄉鎮?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-9',
+        'text': '這個社區單位(醫院、診所)的名稱?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-10',
+        'text': '現在我們是在幾樓?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-11',
+        'text': '請重複這三個名稱,按第一次複述結果計分',
+        'answer': [{
+            'valueInteger': 3
+        }]
+    },
+    {
+        'linkId': 'MMSE-12',
+        'text': '請從100開始連續減7,一直減7直到我說停為止。(每減對一次得一分)',
+        'answer': [{
+            'valueInteger': 5
+        }]
+    },
+    {
+        'linkId': 'MMSE-13',
+        'text': '藍色',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-14',
+        'text': '悲傷',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-15',
+        'text': '火車',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-16',
+        'text': '(拿出手錶)這是什麼?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-17',
+        'text': '(拿出鉛筆)這是什麼?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-18',
+        'text': '請跟我唸一句話『白紙真正寫黑字』',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-19',
+        'text': '請唸一遍並做做看『請閉上眼睛』',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-20',
+        'text': '請用左/右手(非利手)拿這張紙(三步驟指令,每對一步驟得一分)',
+        'answer': [{
+            'valueInteger': 3
+        }]
+    },
+    {
+        'linkId': 'MMSE-21',
+        'text': '請在紙上寫一句語意完整的句子。(含主詞動詞且語意完整的句子)',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-22',
+        'text': '這裡有一個圖形,請在旁邊畫出一個相同的圖形。(兩五邊形,交一四邊形,有兩交點,則給分)',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    }],
+    [{
+        'linkId': 'MMSE-1',
+        'text': '今年是那一年?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-2',
+        'text': '現在是什麼季節?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-3',
+        'text': '今天是幾號?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-4',
+        'text': '今天是禮拜幾?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-5',
+        'text': '現在是那一個月份?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-6',
+        'text': '我們現在是在那一個省?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-7',
+        'text': '我們現在是在那一個縣、市?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-8',
+        'text': '這裡屬於哪一個區或是鄉鎮?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-9',
+        'text': '這個社區單位(醫院、診所)的名稱?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-10',
+        'text': '現在我們是在幾樓?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-11',
+        'text': '請重複這三個名稱,按第一次複述結果計分',
+        'answer': [{
+            'valueInteger': 3
+        }]
+    },
+    {
+        'linkId': 'MMSE-12',
+        'text': '請從100開始連續減7,一直減7直到我說停為止。(每減對一次得一分)',
+        'answer': [{
+            'valueInteger': 5
+        }]
+    },
+    {
+        'linkId': 'MMSE-13',
+        'text': '藍色',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-14',
+        'text': '悲傷',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-15',
+        'text': '火車',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-16',
+        'text': '(拿出手錶)這是什麼?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-17',
+        'text': '(拿出鉛筆)這是什麼?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-18',
+        'text': '請跟我唸一句話『白紙真正寫黑字』',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-19',
+        'text': '請唸一遍並做做看『請閉上眼睛』',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-20',
+        'text': '請用左/右手(非利手)拿這張紙(三步驟指令,每對一步驟得一分)',
+        'answer': [{
+            'valueInteger': 3
+        }]
+    },
+    {
+        'linkId': 'MMSE-21',
+        'text': '請在紙上寫一句語意完整的句子。(含主詞動詞且語意完整的句子)',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-22',
+        'text': '這裡有一個圖形,請在旁邊畫出一個相同的圖形。(兩五邊形,交一四邊形,有兩交點,則給分)',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    }],
+    [{
+        'linkId': 'MMSE-1',
+        'text': '今年是那一年?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-2',
+        'text': '現在是什麼季節?',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-3',
+        'text': '今天是幾號?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-4',
+        'text': '今天是禮拜幾?',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-5',
+        'text': '現在是那一個月份?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-6',
+        'text': '我們現在是在那一個省?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-7',
+        'text': '我們現在是在那一個縣、市?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-8',
+        'text': '這裡屬於哪一個區或是鄉鎮?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-9',
+        'text': '這個社區單位(醫院、診所)的名稱?',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-10',
+        'text': '現在我們是在幾樓?',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-11',
+        'text': '請重複這三個名稱,按第一次複述結果計分',
+        'answer': [{
+            'valueInteger': 2
+        }]
+    },
+    {
+        'linkId': 'MMSE-12',
+        'text': '請從100開始連續減7,一直減7直到我說停為止。(每減對一次得一分)',
+        'answer': [{
+            'valueInteger': 2
+        }]
+    },
+    {
+        'linkId': 'MMSE-13',
+        'text': '藍色',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-14',
+        'text': '悲傷',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-15',
+        'text': '火車',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-16',
+        'text': '(拿出手錶)這是什麼?',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-17',
+        'text': '(拿出鉛筆)這是什麼?',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-18',
+        'text': '請跟我唸一句話『白紙真正寫黑字』',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-19',
+        'text': '請唸一遍並做做看『請閉上眼睛』',
+        'answer': [{
+            'valueInteger': 1
+        }]
+    },
+    {
+        'linkId': 'MMSE-20',
+        'text': '請用左/右手(非利手)拿這張紙(三步驟指令,每對一步驟得一分)',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-21',
+        'text': '請在紙上寫一句語意完整的句子。(含主詞動詞且語意完整的句子)',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    },
+    {
+        'linkId': 'MMSE-22',
+        'text': '這裡有一個圖形,請在旁邊畫出一個相同的圖形。(兩五邊形,交一四邊形,有兩交點,則給分)',
+        'answer': [{
+            'valueInteger': 0
+        }]
+    }]]
 
 @pytest.fixture
 def items():
@@ -225,10 +717,10 @@ def expected_payload():
         'resourceType': 'QuestionnaireResponse',
         'id': '',
         'meta': {
-            'profile': ['http://ltc-ig.fhir.tw/StructureDefinition/LTCQuestionnaireResponseCDR']
+            'profile': []
         },
         'extension': [],
-        'questionnaire': 'http://ltc-ig.fhir.tw/Questionnaire/ltc-questionnaire-cdr',
+        'questionnaire': '',
         'status': 'completed',
         'subject': {
             'reference': 'Patient/ltc-patient-chen-ming-hui'
