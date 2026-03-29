bc1138bdde446914039db4656ce81c41da72ca5f
peter279k <peter279k@gmail.com>
2026-03-29 22:07:21 +0800

    Release 1.8.0

--- fhir_data_generator/__init__.py
+++ fhir_data_generator/__init__.py
@@ -65,7 +65,7 @@ from fhir_data_generator.simple_client.keycloak_client_credentials import Client
 from fhir_data_generator.simple_client.keycloak_authorization_code import AuthorizationCode
 
 
-__version__ = '1.7.0'
+__version__ = '1.8.0'
 __all__ = [
     'Http',
     'CarePlan',
