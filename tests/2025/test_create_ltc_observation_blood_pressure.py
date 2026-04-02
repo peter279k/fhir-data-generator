557899f7f0fd936e694e4f2bc3427310b8254ede
peter279k <peter279k@gmail.com>
2026-04-03 02:03:49 +0800

    Fix observation_ltc_pasport_blood_pressure.py test

--- tests/2025/test_create_ltc_observation_blood_pressure.py
+++ tests/2025/test_create_ltc_observation_blood_pressure.py
@@ -1,7 +1,7 @@
-from fixtures.observation_ltc_pasport_blood_pressure import *
+from fixtures.observation_ltc_blood_pressure import *
 
 
-def test_create(observation_class, profile_urls, status, category_coding, code_coding, subject, effective_datetime, performer, interpretation, note, body_site_coding, method_coding, component, expected_payload):
+def test_create(observation_class, profile_urls, status, category_coding, code_coding, code_text, subject, effective_datetime, performer, component, expected_payload):
     expected = expected_payload
 
     observation_class.set_profile_urls(profile_urls)
@@ -11,20 +11,13 @@ def test_create(observation_class, profile_urls, status, category_coding, code_c
     observation_class.set_category_coding(category_coding)
 
     observation_class.set_code_coding(code_coding)
+    observation_class.set_code_text(code_text)
 
     observation_class.set_subject(subject)
 
     observation_class.set_effective_datetime(effective_datetime)
 
     observation_class.set_performer(performer)
-
-    observation_class.set_interpretation(interpretation)
-
-    observation_class.set_note(note)
-
-    observation_class.set_body_site_coding(body_site_coding)
-
-    observation_class.set_method_coding(method_coding)
 
     observation_class.set_component(component)
 
